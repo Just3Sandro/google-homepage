7a0adc62b7509d289619bcc1eb795dd52eba4b92
Stalobile <73789401+Stalobile@users.noreply.github.com>
2021-04-29 01:11:32 +0200

    GOOGLE
    Petit problème avec le footer mais chut!

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google</title>
+</head>
+
+<body>
+    <div class="recherche">
+    <nav class="nav-bar">
+        <ul class="nav-ul">
+            <li>
+                <a href="https://mail.google.com/mail/&ogbl" target="_blank">Gmail</a>
+            </li>
+            <li>
+                <a href="https://www.google.fr/imghp?hl=fr&ogbl" target="_blank">Images</a>
+            </li>
+            <li>
+                <a href="https://www.google.cl/int1/es-419/about/products" class="dots-app" title="Google Apps"
+                    target="_blank">
+                    <svg class="dots" focusable="false" viewBox="0 0 24 24">
+                        <path
+                            d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z">
+                        </path>
+                    </svg>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/Stalobile/google-homepage" target="_blank">Github</a>
+            </li>
+        </ul>
+    </nav>
+    
+        <div class="logo">
+            <img id="google-logo"
+                src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                alt="Logo GOOGLE">
+
+        </div>
+        <div class="boite-bar">
+
+            <div class="bar">
+                <svg xmlns="http://www.w3.org/2000/svg" id="loupe" width="16" height="16" fill="currentColor"
+                    class="bi bi-search" viewBox="0 0 16 16">
+                    <path
+                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+
+                <input class=" search-input" type="search">
+
+                <svg xmlns="http://www.w3.org/2000/svg" id="mic" width="16" height="16" fill="currentColor"
+                    class="bi bi-mic" role="img" viewBox="0 0 16 16">
+                    <path
+                        d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
+                    <path
+                        d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z" />
+                </svg>
+            </div>
+        </div>
+    <div class="boutons">
+        <button type="submit" class="bouton">Google  Search</button>
+        <button type="submit" class="bouton">I'm Feeling Lucky</button>
+    </div>
+    <div class="langue">
+        <p>Google disponible en : <a href="https://www.google.com/setprefs" target="_blank">Anglais </a> </p>
+    </div>
+    </div>
+<footer>  
+    <div class="pays">FRANCE</div>
+    <div class="foot">
+    <div class="footer-gauche">
+         <a>A propos</a>
+         <a>Publicité</a>
+         <a>Entreprise</a>
+         <a>Comment fonction la recherche Google?</a>
+    </div>
+    <div class="footer-droite">
+    <a>info consommateurs</a>
+    <a>Confidentialité</a>
+    <a>Conditions</a>
+    <a>Paramètres</a>
+
+        <!--<span class="footer-centre"> <a>Neutre en carbone depuis 2007</a></span>-->      
+    </div>
+    </div>
+</footer>
+
+</body>
+
+
+</html>
